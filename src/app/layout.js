19cc6ab75ed9ad125f404b7f118e6a697dcd6669
MaximBrewer <pimax1978@icloud.com>
2023-08-07 21:15:00 +0600

    sdf

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
     <html lang="ru">
       <head>
         <Script
-          src="/data.js?н=5"
+          src="/data.js?v=1.0"
           strategy="beforeInteractive"
         />
       </head>
